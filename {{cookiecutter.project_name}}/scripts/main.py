ba74b909c0af3f9483e01c86224fb05c0464dc40
Alex Button <alex.button.gmx.ch>
2023-05-02 14:28:57 -0700

    resolve initial pre-commit issues

--- {{cookiecutter.project_name}}/scripts/main.py
+++ {{cookiecutter.project_name}}/scripts/main.py
@@ -2,4 +2,6 @@ import sys
 
 sys.path.append("../")
 
-from {{cookiecutter.project_name}} import {{cookiecutter.project_name}}
+from {{cookiecutter.project_name}}.{{cookiecutter.project_name}} import example_function
+
+example_function()
