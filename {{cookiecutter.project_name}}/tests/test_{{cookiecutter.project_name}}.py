45ed6b78c82be56659f8bfac6a08d7b3ce56c467
Alex Button <alex.button.gmx.ch>
2023-05-06 12:59:31 -0700

    updated tests to write test outputs to the test_output directory

--- {{cookiecutter.project_name}}/tests/test_{{cookiecutter.project_name}}.py
+++ {{cookiecutter.project_name}}/tests/test_{{cookiecutter.project_name}}.py
@@ -1,5 +1,15 @@
 from {{cookiecutter.project_name}} import __version__
 
+from tests.helper import add_timestamp
+
+from tests.output import TEST_OUTPUT
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_file():
+    # create timestamped output filename
+    output_filename = add_timestamp(f"{TEST_OUTPUT}/test.txt")
+    # write output file to the test_output directory
+    open(output_filename,"w+") as f:
+        f.write("example")
