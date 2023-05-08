318bb7d183bdee71d582bd1981c263be072e6f48
Alex Button <alex.button.gmx.ch>
2023-05-08 16:52:55 -0700

    added _static and _templates dir

--- {{cookiecutter.project_name}}/docs/source/conf.py
+++ {{cookiecutter.project_name}}/docs/source/conf.py
@@ -41,5 +41,5 @@ exclude_patterns: List[str] = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "python_docs_theme"
 html_static_path = ["_static"]
